19465368cfb94c0843f9a74da0ec74d8a0d62ee3
Ted Gueniche <ted.gueniche@gmail.com>
2014-05-15 18:31:34 +0100

    Minor changes to estimate the number of bi-pattern saving in CPT. A bi-pattern is a set of two consecutive items. So far, it shows we can save up to 1/3 of the nodes in CPT.

--- src/ca/ipredict/controllers/MainController.java
+++ src/ca/ipredict/controllers/MainController.java
@@ -2,10 +2,10 @@ package ca.ipredict.controllers;
 
 import ca.ipredict.database.DatabaseHelper.Format;
 import ca.ipredict.predictor.Evaluator;
+import ca.ipredict.predictor.CPT.CPTPredictor;
 import ca.ipredict.predictor.DG.DGPredictor;
-import ca.ipredict.predictor.Markov.MarkovAllKPredictor;
 import ca.ipredict.predictor.Markov.MarkovFirstOrderPredictor;
-import ca.ipredict.predictor.OLD_CPT.OLD_CPTPredictor;
+import ca.ipredict.predictor.TDAG.TDAGPredictor;
 import ca.ipredict.predictor.profile.Profile;
 
 /**
@@ -17,16 +17,23 @@ public class MainController {
 		
 		//instantiate the evaluator
 		Evaluator evaluator = new Evaluator();
+		
 	
 		//Loading data sets
-		evaluator.addDataset(Format.BMS, 		2000);
-		evaluator.addDataset(Format.SIGN, 		1000);
+		evaluator.addDataset(Format.BMS, 		1000);
+//		evaluator.addDataset(Format.SIGN, 		1000);
+//		evaluator.addDataset(Format.MSNBC, 		1000);
+//		evaluator.addDataset(Format.BIBLE_WORD, 500);
+//		evaluator.addDataset(Format.KOSARAK, 	1000);
 		
 		//Loading predictors
 		evaluator.addPredictor(new DGPredictor());
-		evaluator.addPredictor(new OLD_CPTPredictor());
-		evaluator.addPredictor(new MarkovFirstOrderPredictor());
-		evaluator.addPredictor(new MarkovAllKPredictor());
+//		evaluator.addPredictor(new CPT11_CPTPredictor());
+		evaluator.addPredictor(new TDAGPredictor());
+		evaluator.addPredictor(new CPTPredictor());
+		
+//		evaluator.addPredictor(new MarkovFirstOrderPredictor());
+//		evaluator.addPredictor(new MarkovAllKPredictor());
 		
 		//Start the experiment
 		evaluator.Start(Evaluator.KFOLD, 12, true);
--- src/ca/ipredict/predictor/CPT/CPTPredictor.java
+++ src/ca/ipredict/predictor/CPT/CPTPredictor.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
 
 import ca.ipredict.database.Item;
 import ca.ipredict.database.Sequence;
@@ -50,6 +52,10 @@ public class CPTPredictor implements Predictor {
 		II = new HashMap<Integer, Bitvector>();
 		nodeNumber = 0;
 		
+		//Compression
+		HashMap<List<Item>, Integer> compression = new HashMap<List<Item>, Integer>();
+		
+		
 		int seqId = 0;
 		PredictionTree curNode;
 
@@ -73,7 +79,6 @@ public class CPTPredictor implements Predictor {
 					II.put(item.val, tmpBitset);
 				}
 
-				//TODO: possible improvement: use setBitAndIncrementCardinality() instead of setBit()
 				//updating Inverted Index with seqId for this Item
 				II.get(item.val).setBit(seqId);
 				
@@ -81,6 +86,12 @@ public class CPTPredictor implements Predictor {
 				if(curNode.hasChild(item) == false) {
 					curNode.addChild(item);
 					nodeNumber++;
+					
+					//Generating a pattern for compression
+					List<Item> pattern = new ArrayList<Item>();
+					pattern.add(curNode.Item);
+					pattern.add(item);
+					compression.put(pattern, (compression.get(pattern) == null) ? 1 : (compression.get(pattern) + 1));
 				}
 				curNode = curNode.getChild(item);
 				
@@ -90,6 +101,27 @@ public class CPTPredictor implements Predictor {
 			seqId++; //increment sequence id number
 
 		}
+		
+		
+		
+		
+		int threshold = 3;
+		int patternElligible = 0;
+		int saving = 0;
+		for(Entry<List<Item>, Integer> entry : compression.entrySet()) {
+		
+			if(entry.getValue() > threshold) {
+				patternElligible++;
+				saving += entry.getValue();
+			}
+			
+		}
+		
+		System.out.println("Total pattern: "+ compression.size());
+		System.out.println("Total elligible pattern: "+ patternElligible);
+		System.out.println("Total node saving: "+ saving);
+		
+		
 		return true;
 	}
 
@@ -98,6 +130,8 @@ public class CPTPredictor implements Predictor {
 
 		CPTHelper.predictor = this;
 
+		boolean useRecDiv = true;
+		
 
 		//remove items that were never seen before from the Target sequence before LLCT try to make a prediction
 		//If set to false, those items will be still ignored later on (in updateCountTable())
@@ -111,60 +145,64 @@ public class CPTPredictor implements Predictor {
 		CountTable ct = new CountTable(this);
 		ct.update(target.getItems().toArray(new Item[0]), target.size());
 		
-		int recursion = 0;
-		while(target.size() > recursion && target.size() > recursion && predicted.size() == 0 && recursion < Profile.recursiveDividerMax ) {
 		
-			//Generating all subsequence of size Target.size() - recursion and adding these in the countTable
-			List<Item[]> subsequences = CPTHelper.noiseRemover(new Item[0], target.getItems().toArray(new Item[0]), 0, target.size() - recursion);
-			for(Item[] sub : subsequences) {
-				ct.update(sub, target.size());
+		if(useRecDiv) {
+			int recursion = 0;
+			while(target.size() > recursion && target.size() > recursion && predicted.size() == 0 && recursion < Profile.recursiveDividerMax ) {
+			
+				//Generating all subsequence of size Target.size() - recursion and adding these in the countTable
+				List<Item[]> subsequences = CPTHelper.noiseRemover(new Item[0], target.getItems().toArray(new Item[0]), 0, target.size() - recursion);
+				for(Item[] sub : subsequences) {
+					ct.update(sub, target.size());
+				}
+				
+				//if we reached the minimum level of recursion 
+				//then we try to make a prediction
+				if(recursion >= Profile.recursiveDividerMin)
+					predicted = ct.getBestSequence(1);
+				
+				recursion++;
 			}
 			
-			//if we reached the minimum level of recursion 
-			//then we try to make a prediction
-			if(recursion >= Profile.recursiveDividerMin)
-				predicted = ct.getBestSequence(1);
-			
-			recursion++;
+			//Make a prediction
+			predicted = ct.getBestSequence(1);
 		}
-		
-		//Make a prediction
-		predicted = ct.getBestSequence(1);
-		
-		
-		//CODE TO REMOVE 1 ITEM AT THE TIME BASED ON THE LOWEST COVERAGE
-		//IT GIVE REALLY GOOD TESTING TIME BUT SLIGHLTY LOWER ACCURACY
-//		while(predicted.size() == 0 && target.size() > 1) {
-//			
-//			List<Item> cutSeq = new ArrayList<Item>();
-//			
-//			//Find the lowest supporting item
-//			int minSup = Integer.MAX_VALUE;
-//			int itemVal = -1;
-//			for(Item item : target.getItems()) {
-//				if(II.get(item.val).cardinality() < minSup) {
-//					minSup = II.get(item.val).cardinality();
-//					itemVal = item.val;
-//				}
-//			}
-//			
-//			//Remove the lowest supporting item
-//			for(Item item : target.getItems()) {
-//				if(item.val.equals(itemVal) == false) {
-//					cutSeq.add(item);
-//				}
-//			}
-//
-//			//Updating target sequence
-//			target.getItems().clear();
-//			target.getItems().addAll(cutSeq);
-//			
-//			//Updating the count table
-//			ct.update(target.getItems().toArray(new Item[0]), target.size());
-//			
-//			//Extract the best sequence
-//			predicted = ct.getBestSequence(1);
-//		}
+		else {
+	
+			//CODE TO REMOVE 1 ITEM AT THE TIME BASED ON THE LOWEST COVERAGE
+			//IT GIVE REALLY GOOD TESTING TIME BUT SLIGHLTY LOWER ACCURACY
+			while(predicted.size() == 0 && target.size() > 1) {
+				
+				List<Item> cutSeq = new ArrayList<Item>();
+				
+				//Find the lowest supporting item
+				int minSup = Integer.MAX_VALUE;
+				int itemVal = -1;
+				for(Item item : target.getItems()) {
+					if(II.get(item.val).cardinality() < minSup) {
+						minSup = II.get(item.val).cardinality();
+						itemVal = item.val;
+					}
+				}
+				
+				//Remove the lowest supporting item
+				for(Item item : target.getItems()) {
+					if(item.val.equals(itemVal) == false) {
+						cutSeq.add(item);
+					}
+				}
+	
+				//Updating target sequence
+				target.getItems().clear();
+				target.getItems().addAll(cutSeq);
+				
+				//Updating the count table
+				ct.update(target.getItems().toArray(new Item[0]), target.size());
+				
+				//Extract the best sequence
+				predicted = ct.getBestSequence(1);
+			}
+		}
 		
 		
 		return predicted;
