5da5dc53ddaa9c3a7fb966fe6c3d5b5f55b30d9b
Ted Gueniche <ted.gueniche@gmail.com>
2014-06-22 16:11:32 +0100

    Added empty (to implement) LZ78 predictor

---
+++ src/ca/ipredict/predictor/LZ78/ALZ.java
@@ -0,0 +1,16 @@
+package ca.ipredict.predictor.LZ78;
+
+/**
+ * Predictor based on the Active LeZi algorithm
+ * 
+ * K. Gopalratnam and D. J. Cook. Active Lezi: An incremental parsing algorithm for sequential prediction. 
+ * In Proceedings of the Florida Artiﬁcial Intelligence Research Symposium, 2003.
+ * 
+ * Not implemented yet
+ */
+public class ALZ {
+
+	
+	
+	
+}
