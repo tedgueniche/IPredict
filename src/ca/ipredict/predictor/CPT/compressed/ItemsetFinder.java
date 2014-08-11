625121140525da02f7873bf23775fca9dcf3470c
Ted Gueniche <ted.gueniche@gmail.com>
2014-08-11 01:33:46 +0100

    Implemented a trivial frequent itemset finder.
    Given a dataset of sequences, a min support, a min size and a max size it can find itemset having a high enough support with a length in [min size, max size].
    The algorithm is properly working but can easily be enhanced.
    
    Suggestion: use a known frequent itemset mining algorithm.

---
+++ src/ca/ipredict/predictor/CPT/compressed/ItemsetFinder.java
@@ -0,0 +1,110 @@
+package ca.ipredict.predictor.CPT.compressed;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import ca.ipredict.database.Item;
+import ca.ipredict.database.Sequence;
+
+public class ItemsetFinder {
+
+	
+	
+	/**
+	 * Return all the consecutive items (length between [minLength,maxlength] ) found in the 
+	 * given sequences with a high enough support (support >= minSup)
+	 * @param minLength Minimum length for the itemsets
+	 * @param maxlength Maximum length for the itemsets
+	 */
+	public static List<List<Item>> findFrequentItemsets(List<Sequence> seqs, int minLength, int maxlength, int minSup) {
+		
+		List<List<Item>> frequents = new ArrayList<List<Item>>();
+		HashMap<List<Item>, Integer> frequencies = new HashMap<List<Item>, Integer>();
+		
+		if(maxlength <= 1 || minLength > maxlength) {
+			return frequents;
+		}
+		
+		//Calculating frequencies by iterating through each sequence
+		for(Sequence seq : seqs) {
+			
+			if(seq.size() >= minLength) {
+				
+				for(int i = 0; i < seq.size() - 1; i++) {
+					
+					//Calculate the frequencies of itemsets of size in range [minLength, maxlength]
+					List<Item> itemset = new ArrayList<Item>();
+					for(int offset = i; (offset - i) < maxlength && offset < seq.size(); offset++) {
+						
+						//adding one item at the time to the itemset
+						itemset = new ArrayList<Item>(itemset);
+						itemset.add(seq.get(offset));
+						
+						//saving the frequency of itemset big enough
+						if(itemset.size() >= minLength) {
+							
+							//Updating the frequency of this itemset
+							Integer support = frequencies.get(itemset);
+							if(support == null) {
+								support = 0;
+							}
+							frequencies.put(itemset, support + 1);
+							
+						}
+					}				
+				}
+			}
+		}
+		
+		//Identifying the itemsets with a support high enough
+		frequencies.entrySet().stream().
+			filter(entry -> entry.getValue() >= minSup).
+			forEach(entry -> frequents.add(entry.getKey()));
+		
+		return frequents;
+	}
+	
+	
+	public static void main(String...args) {
+		
+		
+		//Training sequences
+		List<Sequence> training = new ArrayList<Sequence>();
+//		//1 2 3 4
+//		Sequence seq1 = new Sequence(-1);
+//		seq1.addItem(new Item(1));
+//		seq1.addItem(new Item(2));
+//		seq1.addItem(new Item(3));
+//		seq1.addItem(new Item(4));
+//		training.add(seq1);
+		
+		//1 2 3 4
+		Sequence seq2 = new Sequence(-1);
+		seq2.addItem(new Item(1));
+		seq2.addItem(new Item(2));
+		seq2.addItem(new Item(3));
+		seq2.addItem(new Item(4));
+		training.add(seq2);
+		
+		//1 2 3 4
+		Sequence seq3 = new Sequence(-1);
+		seq3.addItem(new Item(1));
+		seq3.addItem(new Item(2));
+		seq3.addItem(new Item(3));
+		seq3.addItem(new Item(4));
+		training.add(seq3);
+		
+//		//0 1 2 4
+//		Sequence seq4 = new Sequence(-1);
+//		seq4.addItem(new Item(0));
+//		seq4.addItem(new Item(1));
+//		seq4.addItem(new Item(2));
+//		seq4.addItem(new Item(4));
+//		training.add(seq4);
+		
+		
+		System.out.println(findFrequentItemsets(training,2,4,2));
+	}
+	
+}
