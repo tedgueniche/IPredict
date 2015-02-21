ae73d41cd267e0c984554bdb542f0c4578614336
Ted Gueniche <ted.gueniche@gmail.com>
2015-02-20 23:10:59 -0400

    * Updated the main controller for the change of name of CPT and CPT+. * Removed dev controller and tests * Updated .gitignore for release folder

--- src/ca/ipredict/controllers/MainController.java
+++ src/ca/ipredict/controllers/MainController.java
@@ -2,10 +2,8 @@ package ca.ipredict.controllers;
 
 import ca.ipredict.database.DatabaseHelper.Format;
 import ca.ipredict.predictor.Evaluator;
-import ca.ipredict.predictor.CPT.CPTPredictor;
-import ca.ipredict.predictor.CPT.compressed.CPTPredictorCompressed;
-import ca.ipredict.predictor.CPT2013.CPT13FastPredictor;
-import ca.ipredict.predictor.CPT2013.CPT13Predictor;
+import ca.ipredict.predictor.CPT.CPT.CPTPredictor;
+import ca.ipredict.predictor.CPT.CPTPlus.CPTPlusPredictor;
 import ca.ipredict.predictor.DG.DGPredictor;
 import ca.ipredict.predictor.LZ78.LZ78Predictor;
 import ca.ipredict.predictor.Markov.MarkovAllKPredictor;
@@ -24,27 +22,25 @@ public class MainController {
 		Evaluator evaluator = new Evaluator();
 		
 		//Loading data sets
-//		evaluator.addDataset(Format.BMS, 		5000);
+		evaluator.addDataset(Format.BMS, 		5000);
 		evaluator.addDataset(Format.SIGN, 		1000);
-//		evaluator.addDataset(Format.MSNBC, 		5000);
-//		evaluator.addDataset(Format.BIBLE_WORD, 5000);
-//		evaluator.addDataset(Format.BIBLE_CHAR, 5000);
-//		evaluator.addDataset(Format.KOSARAK, 	20000);
-//		evaluator.addDataset(Format.FIFA, 		5000);
+		evaluator.addDataset(Format.MSNBC, 		5000);
+		evaluator.addDataset(Format.BIBLE_WORD, 5000);
+		evaluator.addDataset(Format.BIBLE_CHAR, 5000);
+		evaluator.addDataset(Format.KOSARAK, 	20000);
+		evaluator.addDataset(Format.FIFA, 		5000);
 		
 		//Loading predictors
 		evaluator.addPredictor(new DGPredictor());
-//		evaluator.addPredictor(new TDAGPredictor());
-		evaluator.addPredictor(new CPTPredictorCompressed());
-//		evaluator.addPredictor(new CPTPredictor());
-		evaluator.addPredictor(new CPT13FastPredictor());
-//		evaluator.addPredictor(new CPT13Predictor());
-//		evaluator.addPredictor(new MarkovFirstOrderPredictor());
-//		evaluator.addPredictor(new MarkovAllKPredictor());
+		evaluator.addPredictor(new TDAGPredictor());
+		evaluator.addPredictor(new CPTPlusPredictor("CPT+",		"CCF:true CBS:true"));
+		evaluator.addPredictor(new CPTPredictor());
+		evaluator.addPredictor(new MarkovFirstOrderPredictor());
+		evaluator.addPredictor(new MarkovAllKPredictor());
 		evaluator.addPredictor(new LZ78Predictor());
 		
 		//Start the experiment
-		evaluator.Start(Evaluator.KFOLD, 8 , true);
+		evaluator.Start(Evaluator.KFOLD, 14 , true);
 //		evaluator.Start(Evaluator.HOLDOUT, 0.75f , true);
 		
 		//Shows the parameters used
